af428201b0bbf7293f44f01486c3f50f49f86879
Steven Ly <1stevenly92@gmail.com>
2020-06-12 10:35:16 -0700

    going to attempt to add swagger

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using AnimalShelter.Models;
+using Microsoft.OpenApi.Models;
 
 namespace AnimalShelter
 {
@@ -30,12 +31,21 @@ namespace AnimalShelter
             services.AddDbContext<AnimalShelterContext>(opt =>
                 opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            
+            services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Animal Shelter", Version = "v1" });
+    });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
