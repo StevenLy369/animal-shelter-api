95229a7d601045a4c6b28b6390e57f31ce4c80ad
Steven Ly <1stevenly92@gmail.com>
2020-06-12 10:00:53 -0700

    got put to work

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -7,53 +7,61 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace AnimalShelter.Controller
 {
- 
-  [Route("api/[controller]")]
-  [ApiController]
-  public class AnimalsController : ControllerBase
-  {
-    private AnimalShelterContext _db;
 
-    public AnimalsController(AnimalShelterContext db)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
     {
-      _db = db;
-    }
+        private AnimalShelterContext _db;
+
+        public AnimalsController(AnimalShelterContext db)
+        {
+            _db = db;
+        }
 
 
-    // GET api/animals
-       
-    [HttpGet]
-    public ActionResult<IEnumerable<Animal>> Get(string name, string details, string breed)
-    {
-      var query = _db.Animals.AsQueryable();
-      if (name != null)
-      {
-        query = query.Where(entry => entry.Name == name);
-      }
-      if (details != null)
-      {
-        query = query.Where(entry => entry.Details == details);
-      }
-      if (breed != null)
-      {
-        query = query.Where(entry => entry.Breed == breed);
-      }
-      return query.ToList();
-    }
-    //api/animals/{id}
-    [HttpGet("{id}")]
-    public ActionResult<Animal> GetAction(int id)
-    {
-      return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
-    }
+        // GET api/animals
 
-      // POST api/animals
-    [HttpPost]
-    public void Post([FromBody] Animal animal)
-    {
-      _db.Animals.Add(animal);
-      _db.SaveChanges();
-    }
+        [HttpGet]
+        public ActionResult<IEnumerable<Animal>> Get(string name, string details, string breed)
+        {
+            var query = _db.Animals.AsQueryable();
+            if (name != null)
+            {
+                query = query.Where(entry => entry.Name == name);
+            }
+            if (details != null)
+            {
+                query = query.Where(entry => entry.Details == details);
+            }
+            if (breed != null)
+            {
+                query = query.Where(entry => entry.Breed == breed);
+            }
+            return query.ToList();
+        }
+        //api/animals/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Animal> GetAction(int id)
+        {
+            return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+        }
 
-  }
+        // POST api/animals
+        [HttpPost]
+        public void Post([FromBody] Animal animal)
+        {
+            _db.Animals.Add(animal);
+            _db.SaveChanges();
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Animal animal)
+
+        {
+            animal.AnimalId = id;
+            _db.Entry(animal).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+
+    }
 }
